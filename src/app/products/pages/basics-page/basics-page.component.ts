a57e8aa39c30d8d738646057f1825ada8eebe08b
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2023-11-23 17:32:49 -0600

    Instalando PrimeFlex y dando forma algunas columnas del basic page

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./basics-page.component.css']
 })
 export class BasicsPageComponent {
-
+  public nameLower: string = 'alexander';
+  public nameUpper: string = 'ALEXANDER';
+  public fullName: string = 'eDwIn aLeXanDEr';
 }
