270470bd38c638aac4c356bf68c0d2ef5b95c8fc
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2024-01-02 16:26:58 -0600

    Pipe i18nSelect, es un sistema de internacionalización de Angular para seleccionar y mostrar traducciones basadas en una expresión condicional

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  // i18n Select
+  public name:string = 'Benjamin';
+  public gender: 'male' | 'female' = 'male';
+
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla'
+  }
+
+  changeClient():void{
+    this.name = 'Marilin';
+    this.gender = 'female';
+  }
+
 }
